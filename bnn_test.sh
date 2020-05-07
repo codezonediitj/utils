8f7f04b2c18ecd111e6ce3d1fb028acf9ec77719
Gagandeep Singh <singh.23@iitj.ac.in>
2020-05-08 01:26:15 +0530

    Update bnn_test.sh

--- bnn_test.sh
+++ bnn_test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-# Pass the path to build directory in the arguments.
-./$1/bin/test_autodiff
-./$1/bin/test_core
-./$1/bin/test_operations
+# Pass the first argument as path to build directory in the arguments.
+# Pass the second argument one of, --CI=ON or --CI=OFF.
+./$1/bin/test_autodiff $2
 ./$1/bin/test_cuda_core
-
+./$1/bin/test_operations $2
+./$1/bin/test_core $2
+./$1/bin/test_io $2
